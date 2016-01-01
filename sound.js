ab4e0c2be38b71de7daee258bc952c9077e53d07
tokoroten <shinta.nakayama+github@gmail.com>
2016-01-02 06:13:23 +0900

    5beats drum

--- sound.js
+++ sound.js
@@ -4,30 +4,75 @@
 // http://twilightdve.hatenablog.com/entry/2014/08/12/180221
 
 var context;
+var bpm = 72;
+var beats = 5;
+
+var sampling_rate = null;
+var drum_length = 0.02;
+var drum_source = null;
+var last_drum = null;
+var interval_time = 1;
+var drum_count = 0;
+var interval_id = null;
 
 var init = function() {
     try{
-        context = new webkitAudioContext();
+        context = new AudioContext();
     } catch(e) {
         console.log(e);
     }
-    context.samplingRate = 48000;
+    sampling_rate = parseInt( context.sampleRate);
+
+    // 10秒分のノイズドラムのソースを作る
+    drum_source = context.createBuffer(2, sampling_rate * 10, sampling_rate);
+    var noise_param = 20;
+    for (var c = 0; c < 2;c++)
+    {
+        var channel = drum_source.getChannelData(c);
+        for (var i = 0; i < channel.length; i++) {
+            switch(i % noise_param){
+                case 0:
+                    channel[i] = Math.random() * 2 - 1;
+                    break;
+                default:
+                    channel[i] = channel[i - i % noise_param];
+            }
+        }
+    }
+
+    interval_id = setInterval(update, interval_time / 2 * 1000);
 };
 
-var audio = function(){
-    var buffer = context.createBuffer( 1, 48000, 48000 );
-    var channel = buffer.getChannelData(0);
-    for( var i=0; i < channel.length; i++ )
-    {
-        channel[i] = Math.sin( i / 100 * Math.PI);
+var update = function() {
+    var t = 60.0 / bpm / beats;
+    console.log('hoge',  context.currentTime);
+
+    if(last_drum){
+        var dt = last_drum + t - context.currentTime;
+    }else{
+        var dt = 0;
     }
-    var src = context.createBufferSource();
-    src.buffer = buffer;
-    src.connect(context.destination);
 
-    // そして再生。
-    src.start(context.currentTime);
-}
+    for(; dt < interval_time; dt += t ){
+        if(dt < 0){
+            continue;
+        }
+        last_drum = context.currentTime + dt;
 
-document.querySelector("#play").addEventListener("click", function(){audio();}, false);
-init();
+        var drum_source_node = context.createBufferSource();
+        drum_source_node.buffer = drum_source;
+
+        var gain_node = context.createGain();
+        gain_node.gain.value =drum_count % 4 ==0 ? 1.0 : 0.3;
+
+        drum_source_node.connect(gain_node);
+        gain_node.connect(context.destination);
+        drum_source_node.start(last_drum, Math.random() * 9.0, drum_length);
+
+        drum_count += 1;
+    }
+};
+
+document.querySelector("#play").addEventListener("click", function(){init();}, false);
+document.querySelector("#stop").addEventListener("click", function(){clearInterval(interval_id)}, false);
+
